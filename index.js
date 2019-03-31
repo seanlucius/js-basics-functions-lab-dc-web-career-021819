c83ff42ffa1b1acdfd35c09d6708d7b6ec367373
seanlucius <seanlucius22@icloud.com>
2019-03-31 17:20:23 -0400

    js-functions-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  if (pickup < 42) {
+    return 42 - pickup
+  } else if (pickup > 42) {
+    return pickup - 42
+  }
+}
+
+function distanceFromHqInFeet(pickup) {
+  let blocks = distanceFromHqInBlocks(pickup)
+  return blocks * 264
+}
+
+function distanceTravelledInFeet(start, finish) {
+  let difference = finish > start ? finish - start : start - finish
+  return difference * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination)
+  let fare;
+  if (distance < 400) {
+    fare = 0;
+  } else if (distance < 2000) {
+    fare = (distance - 400) * 0.02
+  } else if (distance < 2500) {
+    fare = 25
+  } else {
+    fare = 'cannot travel that far'
+  }
+  return fare;
+}
